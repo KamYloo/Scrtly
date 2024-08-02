03e790e5a1434d2d871fe089d2a6b1a67cd1c3a2
KamilMieczkowski <kamil.mieczkowski7@wp.pl>
2024-08-02 21:56:38 +0200

    adding playlist view in left menu

--- scrtlyFrontend/src/Components/LeftMenu.jsx
+++ scrtlyFrontend/src/Components/LeftMenu.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import '../Styles/LeftMenu.css'
 
 import { FaSpotify, FaEllipsisH } from "react-icons/fa";
-import {BiSearchAlt} from "react-icons/bi";
+import { BiSearchAlt } from "react-icons/bi";
 import { Menu } from './Menu';
 import { MenuList } from './MenuList';
- 
+import { MenuPlayList } from './MenuPlayList';
+
 function LeftMenu() {
     return (
         <div className='LeftMenu'>
@@ -15,11 +16,12 @@ function LeftMenu() {
                 <i><FaEllipsisH /></i>
             </div>
             <div className="searchBox">
-                <input type="text" placeholder='Search...'/>
-                <i className='searchIcon'><BiSearchAlt/></i>
+                <input type="text" placeholder='Search...' />
+                <i className='searchIcon'><BiSearchAlt /></i>
             </div>
 
-            <Menu title={'Menu'} menuObject={MenuList}/>
+            <Menu title={'Menu'} menuObject={MenuList} />
+            <MenuPlayList />
         </div>
     )
 }
---
+++ scrtlyFrontend/src/Components/MenuPlayList.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { FaPlus } from 'react-icons/fa'
+import { BsMusicNoteList, BsTrash } from 'react-icons/bs'
+import { Playlist } from './PlayList'
+
+function MenuPlayList() {
+    return (
+        <div className='playListBox'>
+            <div className='nameBox'>
+                <p>Playlist</p>
+                <i><FaPlus /></i>
+            </div>
+            <div className="scrollBox">
+
+                {
+                    Playlist && Playlist.map((playList)=> (
+                    <div className="playList">
+                        <i className='iconP'><BsMusicNoteList /></i>
+                        <p>{playList.name}</p>
+                        <i className='trash'><BsTrash /></i>
+                    </div>
+                    ))
+                }
+
+                
+            </div>
+        </div>
+    )
+}
+
+export { MenuPlayList }
