a113a940af54a0f6b28aa3bf2f884325f3635f15
KamilMieczkowski <kamil.mieczkowski7@wp.pl>
2024-08-10 22:46:01 +0200

    added functionality to add favorite songs and change the view of the active song

--- scrtlyFrontend/src/Components/AudioList.jsx
+++ scrtlyFrontend/src/Components/AudioList.jsx
@@ -1,8 +1,34 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { FaHeadphones, FaHeart, FaRegClock, FaRegHeart } from 'react-icons/fa'
 import { Songs } from './Songs'
 
 function AudioList() {
+    const [songs, setSongs] = useState(Songs)
+    const [song, setSong] = useState(Songs[0].song)
+    const [img, setImage] = useState(Songs[0].imgSrc)
+    const [isFavourite, setFavourite] = useState(false)
+
+    useEffect(() => {
+        const songs = document.querySelectorAll(".songs")
+
+        function changeActive() {
+            songs.forEach((n) => n.classList.remove("active"))
+            this.classList.add("active")
+        }
+
+        songs.forEach((n) => n.addEventListener("click", changeActive))
+    }, [])
+
+
+    const changeFavourite = (id) => {
+        Songs.forEach((song) => {
+            if (song.id == id) {
+                song.favourite = !song.favourite
+            }
+        })
+        setSongs([...songs])
+    }
+
     return (
         <div className='audioList'>
             <h2 className="title">
@@ -33,7 +59,7 @@ function AudioList() {
                                             02.47
                                         </p>
 
-                                        <div className="favourite">
+                                        <div className="favourite" onClick={() => changeFavourite(song?.id)}>
                                             {
                                                 song?.favourite ?
                                                     <i><FaHeart /></i>
