09e3039bb5d442fba69fe98a2974067ba9c284be
KamilMieczkowski <kamil.mieczkowski7@wp.pl>
2024-08-10 13:15:33 +0200

    added menulist in mainBox/styles

--- scrtlyFrontend/src/Components/Middle.jsx
+++ scrtlyFrontend/src/Components/Middle.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import '../Styles/Middle.css'
 import { Banner } from './Banner'
+import { FaUsers } from 'react-icons/fa'
 
 
 
 function Middle() {
+  useEffect(() => {
+    const allLi = document.querySelector(".menuList").querySelectorAll("li")
+
+    function changeManeuActive() {
+      allLi.forEach((n) => n.classList.remove("active"))
+      this.classList.add("active")
+    }
+
+    allLi.forEach((n) => n.addEventListener("click", changeManeuActive))
+  }, [])
+
   return (
     <div className='mainBox'>
       <Banner />
+
+      <div className="menuList">
+        <ul>
+          <li><a href="#">Popular</a></li>
+          <li><a href="#">Albums</a></li>
+          <li><a href="#">Songs</a></li>
+          <li><a href="#">Fans</a></li>
+          <li><a href="#">About</a></li>
+        </ul>
+        <p><i><FaUsers /></i>12.3M <span>Followers</span></p>
+      </div>
     </div>
   )
 }
