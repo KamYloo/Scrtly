9331918d64e0ca7bf0a7aa9d3abbeb164c21fbcf
KamilMieczkowski <kamil.mieczkowski7@wp.pl>
2024-08-18 18:55:43 +0200

    setup backend project using springBoot and setting up a connection with a database

---
+++ Scrtly-backend/src/main/java/com/kamylo/Scrtly_backend/ScrtlyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kamylo.Scrtly_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScrtlyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScrtlyBackendApplication.class, args);
+	}
+
+}
