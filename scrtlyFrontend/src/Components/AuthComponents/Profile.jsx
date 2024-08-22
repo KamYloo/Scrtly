686af0056bda16fdc2a0e9736ca53141ca85b4da
KamilMieczkowski <kamil.mieczkowski7@wp.pl>
2024-08-22 22:39:41 +0200

    added profileView/style/responsiveness funkcionality loaded userData from backend

--- scrtlyFrontend/src/Components/AuthComponents/Profile.jsx
+++ scrtlyFrontend/src/Components/AuthComponents/Profile.jsx
@@ -1,8 +1,55 @@
 import React from 'react'
+import "../../Styles/Profile.css"
+import { ProfileInfo } from './ProfileInfo'
 
 function Profile() {
     return (
-        <div>Profile</div>
+        <div className='profileSite'>
+            <ProfileInfo />
+            <hr />
+            <div className="posts">
+                <div className="post">
+                    <img src="" alt="" />
+                </div>
+                <div className="post">
+                    <img src="" alt="" />
+                </div>
+                <div className="post">
+                    <img src="" alt="" />
+                </div>
+                <div className="post">
+                    <img src="" alt="" />
+                </div>
+                <div className="post">
+                    <img src="" alt="" />
+                </div>
+                <div className="post">
+                    <img src="" alt="" />
+                </div>
+                <div className="post">
+                    <img src="" alt="" />
+                </div><div className="post">
+                    <img src="" alt="" />
+                </div><div className="post">
+                    <img src="" alt="" />
+                </div><div className="post">
+                    <img src="" alt="" />
+                </div><div className="post">
+                    <img src="" alt="" />
+                </div><div className="post">
+                    <img src="" alt="" />
+                </div><div className="post">
+                    <img src="" alt="" />
+                </div><div className="post">
+                    <img src="" alt="" />
+                </div><div className="post">
+                    <img src="" alt="" />
+                </div><div className="post">
+                    <img src="" alt="" />
+                </div>
+                
+            </div>
+        </div>
     )
 }
 
---
+++ scrtlyFrontend/src/Components/AuthComponents/ProfileInfo.jsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import "../../Styles/Profile.css"
+import { IoIosSettings } from "react-icons/io";
+
+function ProfileInfo() {
+    const [user, setUser] = useState({ fullName: '', email: '' });
+
+    useEffect(() => {
+
+        const token = localStorage.getItem('jwtToken')
+
+        if (token) {
+            axios.get('http://localhost:8080/auth/me', {
+                headers: {
+                    Authorization: `Bearer ${token}`
+                }
+            })
+                .then(response => {
+                    setUser(response.data);
+                })
+                .catch(error => {
+                    console.error("Error fetching user info:", error);
+                });
+        }
+    }, []);
+
+    return (
+        <div className='profileInfo'>
+            <div className="userData">
+                <img src="" alt="" />
+                <div className="right">
+                    <div className="top">
+                        <p>{user.fullName || 'Name Surname'}</p>
+                        <button>Edit Profile</button>
+                        <i><IoIosSettings /></i>
+                    </div>
+                    <div className="stats">
+                        <p>Posts: 45</p>
+                        <p>99 followers</p>
+                        <p>Following: 155</p>
+                    </div>
+                    <div className="description">
+                        <p>Name</p>
+                        <span>gafgadhgdahfhgadvfavafdaerv</span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export { ProfileInfo }
