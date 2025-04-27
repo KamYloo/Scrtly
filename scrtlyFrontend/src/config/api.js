8a7251a406fb6f88ee346bab696fbc95285c50d7
Kamil Mieczkowski <kamil.mieczkowski7@gmail.com>
2025-04-27 18:10:30 +0200

    udpate frontend config to prod

--- scrtlyFrontend/src/config/api.js
+++ scrtlyFrontend/src/config/api.js
@@ -1,6 +1,4 @@
-export const BASE_API_URL = import.meta.env.DEV
-    ? ''
-    : import.meta.env.VITE_APP_BACKEND_URL;
+export const BASE_API_URL = import.meta.env.VITE_APP_BACKEND_URL;
 
 export const fetchWithAuth = async (url, options = {}, errorType) => {
     let headers = {
--- scrtlyFrontend/vite.config.js
+++ scrtlyFrontend/vite.config.js
@@ -3,17 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://145.239.91.66:5001',
-        changeOrigin: true,
-      },
-      '/ws': {
-        target: 'ws://145.239.91.66:5001',
-        ws: true
-      }
-    }
-  }
 })
