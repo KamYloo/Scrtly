313e7b553ae5035c051e6fc49cacd042a781f46c
Kamil Mieczkowski <kamil.mieczkowski7@gmail.com>
2025-04-27 14:53:27 +0200

    udpate frontend config to prod

--- scrtlyFrontend/src/config/api.js
+++ scrtlyFrontend/src/config/api.js
@@ -1,6 +1,6 @@
 export const BASE_API_URL = import.meta.env.DEV
     ? ''
-    : import.meta.env.VITE_API_BASE_URL;
+    : import.meta.env.VITE_APP_BACKEND_URL;
 
 export const fetchWithAuth = async (url, options = {}, errorType) => {
     let headers = {
