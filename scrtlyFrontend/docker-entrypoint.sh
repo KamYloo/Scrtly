bde5720e45582486fcdfd69df901338229bcb5aa
KamilMieczkowski <kamil.mieczkowski7@gmail.com>
2025-08-18 14:13:24 +0200

    docker-entrypoint update

--- scrtlyFrontend/docker-entrypoint.sh
+++ scrtlyFrontend/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cat > /app/dist/env.js <<EOF
-window.__env = {
+window.__ENV = {
   VITE_APP_BACKEND_URL: "${VITE_APP_BACKEND_URL:-}",
   VITE_STRIPE_PUBLIC_KEY: "${VITE_STRIPE_PUBLIC_KEY:-}"
 };
