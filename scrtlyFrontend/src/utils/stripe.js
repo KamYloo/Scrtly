c7eb7a1a0e4394755ff817e6dcbd52d7e7578a7a
KamilMieczkowski <kamil.mieczkowski7@gmail.com>
2025-08-18 14:56:11 +0200

    stripe.js update

--- scrtlyFrontend/src/utils/stripe.js
+++ scrtlyFrontend/src/utils/stripe.js
@@ -1,7 +1,8 @@
 import { loadStripe } from "@stripe/stripe-js";
-const stripeKey = window.__ENV?.VITE_STRIPE_PUBLIC_KEY;
+const stripeKey = "pk_test_51RiZalFy63ngC4AgptAl0yXRIOdi3VdvgcepZMXeZ29Kvxnu4E8dHnB9eRvEij1Xg08T8XbUHg4I1sYQG4LKFwVB002tV01mDV";
 
 if (!stripeKey) {
-    console.error("Stripe public key is missing!", window.__ENV);
+    console.error("Stripe public key is missing!");
 }
+
 export const stripePromise = loadStripe(stripeKey);
