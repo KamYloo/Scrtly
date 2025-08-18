0b036e3250984b596f628d816f2b4a59ea4d3888
KamilMieczkowski <kamil.mieczkowski7@gmail.com>
2025-08-18 14:47:11 +0200

    stripe.js update

--- scrtlyFrontend/src/utils/stripe.js
+++ scrtlyFrontend/src/utils/stripe.js
@@ -1,3 +1,7 @@
 import { loadStripe } from "@stripe/stripe-js";
-const stripeKey = window.__ENV?.VITE_STRIPE_PUBLIC_KEY || import.meta.env.VITE_STRIPE_PUBLIC_KEY;
+const stripeKey = window.__ENV?.VITE_STRIPE_PUBLIC_KEY;
+
+if (!stripeKey) {
+    console.error("Stripe public key is missing!", window.__ENV);
+}
 export const stripePromise = loadStripe(stripeKey);
