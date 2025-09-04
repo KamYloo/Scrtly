dc94921cb4e9a6a16bedbed027f5c297fd678c83
KamilMieczkowski <kamil.mieczkowski7@gmail.com>
2025-09-04 12:19:17 +0200

    improved validation of playListController endpoints

--- Scrtly-backend/src/main/java/com/kamylo/Scrtly_backend/playList/web/controller/PlayListController.java
+++ Scrtly-backend/src/main/java/com/kamylo/Scrtly_backend/playList/web/controller/PlayListController.java
@@ -1,14 +1,19 @@
 package com.kamylo.Scrtly_backend.playList.web.controller;
 
 import com.kamylo.Scrtly_backend.playList.web.dto.PlayListDto;
+import com.kamylo.Scrtly_backend.playList.web.dto.request.PlayListCreateRequest;
+import com.kamylo.Scrtly_backend.playList.web.dto.request.PlayListUpdateRequest;
 import com.kamylo.Scrtly_backend.song.web.dto.SongDto;
 import com.kamylo.Scrtly_backend.common.response.PagedResponse;
 import com.kamylo.Scrtly_backend.playList.service.PlayListService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
 import lombok.AllArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -21,12 +26,11 @@ public class PlayListController {
 
     private final PlayListService playListService;
 
-    @PostMapping("/create")
-    public ResponseEntity<PlayListDto> createPlayList(@RequestParam("file") MultipartFile file,
-                                                      @RequestParam("title") String title,
+    @PostMapping(value = "/create", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<PlayListDto> createPlayList(@ModelAttribute @Valid PlayListCreateRequest request,
                                                       Principal principal) {
 
-        PlayListDto playList = playListService.createPlayList(title, principal.getName(), file);
+        PlayListDto playList = playListService.createPlayList(request.getTitle(), principal.getName(), request.getFile());
         return new ResponseEntity<>(playList, HttpStatus.CREATED);
     }
 
@@ -43,46 +47,47 @@ public class PlayListController {
     }
 
     @GetMapping("/{playListId}")
-    public ResponseEntity<PlayListDto> getPlayList(@PathVariable Integer playListId) {
+    public ResponseEntity<PlayListDto> getPlayList(@PathVariable @Positive(message = "{id.positive}") Integer playListId) {
 
         PlayListDto playList = playListService.getPlayList(playListId);
         return new ResponseEntity<>(playList, HttpStatus.OK);
     }
 
     @GetMapping("/{playListId}/tracks")
-    public ResponseEntity<PagedResponse<SongDto>> getPlayListTracks(@PathVariable Integer playListId, @PageableDefault(size = 10) Pageable pageable) {
+    public ResponseEntity<PagedResponse<SongDto>> getPlayListTracks(@PathVariable @Positive(message = "{id.positive}") Integer playListId, @PageableDefault(size = 10) Pageable pageable) {
         Page<SongDto> songs = playListService.getPlayListTracks(playListId, pageable);
         return new ResponseEntity<>(PagedResponse.of(songs), HttpStatus.OK);
     }
 
     @PutMapping("/{playListId}/addSong/{songId}")
-    public ResponseEntity<PlayListDto> addSongToPlayList(@PathVariable("playListId") Integer playListId,
-                                                                @PathVariable("songId") Long songId,
-                                                                Principal principal)  {
+    public ResponseEntity<PlayListDto> addSongToPlayList(
+            @PathVariable("playListId") @Positive(message = "{id.positive}") Integer playListId,
+            @PathVariable("songId") @Positive(message = "{id.positive}") Long songId,
+            Principal principal)  {
 
         PlayListDto playList = playListService.addSongToPlayList(songId, playListId, principal.getName());
         return new ResponseEntity<>(playList, HttpStatus.OK);
     }
 
     @DeleteMapping("/{playListId}/deleteSong/{songId}")
-    public ResponseEntity<?> deleteSongFromPlayList(@PathVariable("playListId") Integer playListId,
-                                                              @PathVariable("songId") Long songId,
-                                                              Principal principal) {
+    public ResponseEntity<?> deleteSongFromPlayList(
+            @PathVariable("playListId") @Positive(message = "{id.positive}") Integer playListId,
+            @PathVariable("songId") @Positive(message = "{id.positive}") Long songId,
+            Principal principal) {
         playListService.removeSongFromPlayList(songId, playListId, principal.getName());
         return ResponseEntity.ok(songId);
     }
 
     @PutMapping("/update")
-    public ResponseEntity<PlayListDto> updatePlayList(Integer playListId, String title,
-                                                      @RequestPart(value = "file", required = false) MultipartFile file,
+    public ResponseEntity<PlayListDto> updatePlayList(@ModelAttribute @Valid PlayListUpdateRequest request,
                                                       Principal principal) {
 
-        PlayListDto updatePlayList = playListService.updatePlayList(playListId, title, principal.getName(), file);
+        PlayListDto updatePlayList = playListService.updatePlayList(request.getPlayListId(), request.getTitle(), principal.getName(), request.getFile());
         return new ResponseEntity<>(updatePlayList, HttpStatus.OK);
     }
 
     @DeleteMapping("/delete/{playListId}")
-    public ResponseEntity<?> deletePlayList(@PathVariable Integer playListId, Principal principal) {
+    public ResponseEntity<?> deletePlayList(@PathVariable @Positive(message = "{id.positive}") Integer playListId, Principal principal) {
        playListService.deletePlayList(playListId, principal.getName());
        return ResponseEntity.ok(playListId);
     }
---
+++ Scrtly-backend/src/main/java/com/kamylo/Scrtly_backend/playList/web/dto/request/PlayListUpdateRequest.java
@@ -0,0 +1,27 @@
+package com.kamylo.Scrtly_backend.playList.web.dto.request;
+
+import com.kamylo.Scrtly_backend.common.config.FileConstraint;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class PlayListUpdateRequest {
+    @Positive(message = "{id.positive}")
+    private Integer playListId;
+
+    @NotBlank(message = "{playlist.title.notblank}")
+    @Size(max = 30, message = "{playlist.title.size}")
+    private String title;
+
+    @NotNull(message = "{playlist.cover.required}")
+    @FileConstraint(allowed = {"image/png","image/jpeg"}, maxSizeKb = 10240, message = "{image.tooLarge}")
+    private MultipartFile file;
+}
